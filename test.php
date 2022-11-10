d2d0ccc7a7ea22215e30e96fdcacfea71d9c64f1
Shirayuki3012 <117940419+Shirayuki3012@users.noreply.github.com>
2022-11-11 02:00:45 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,7 @@
+echo "# shira" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Shirayuki3012/shira.git
+git push -u origin main
